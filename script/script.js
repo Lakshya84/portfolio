40e022e83b632a45658402bb7816677dddbc0c9b
Lakshya84 <lakshyapandey787@gmail.com>
2024-09-10 03:01:07 +0530

    improved user experience by applying locomotive.js to achieve smooth scrolling and transitions

--- script/script.js
+++ script/script.js
@@ -1,30 +1,5 @@
-function page2Anim() {
-  gsap.registerPlugin(ScrollTrigger);
-
-  const moveElement = document.querySelector(".move");
-  const marqElements = document.querySelectorAll(".marq");
-  const marqWidth = marqElements[0].offsetWidth;
-  const totalWidth = marqWidth * marqElements.length;
-  
-  // Calculate endOffset correctly, considering innerWidth
-  const endOffset = totalWidth - window.innerWidth;
-
-  let timeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: "#page-2",
-      start: "top top",
-      end: `+=${endOffset}`,
-      scrub: 1,
-      pin: true,
-      anticipatePin: 1,
-    },
-  });
-
-  timeline.to(moveElement, {
-    x: -totalWidth + window.innerWidth / 2,
-    ease: "power2.inOut",
-  });
-}
-
-
-// page2Anim();
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+  lerp: "0.08"
+});
